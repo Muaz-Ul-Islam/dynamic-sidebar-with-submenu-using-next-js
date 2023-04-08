083f58e7e7bc034cb8fd54963d5050abdab9ce20
Muaz-Ul-Islam <muazch1122@gmail.com>
2023-04-08 06:26:48 +0500

    sidebar structure ready...
    dynamic sidebar ready to use and initial pages created + needed packages installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import SideBarComp from '@/components/Sections/SharedSections/SidebarComp/SideBarComp'
 import '@/styles/globals.css'
+import { Grid } from '@mui/material'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Grid container >
+
+      <Grid item xs={1.7} >
+        <SideBarComp />
+      </Grid>
+      <Grid item xs={10.3} >
+        <Component {...pageProps} />
+
+      </Grid>
+
+
+    </Grid>
+
+  </>
 }
+
